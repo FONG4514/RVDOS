ba82b0212db94d6b5b916fb074608406caf52097
FONG4514 <1220373103@qq.com>
2026-05-04 19:40:40 +0800

    refactor: 0.7.6更新,将page的分配从空闲链表更换为位图

--- kernelspace/include/param.h
+++ kernelspace/include/param.h
@@ -4,6 +4,6 @@
 #define MAXCPUCORE 8
 #define MAXPROCESSES 64
 
-#define KERNEL_VERSION "Alpha-0.7.5-Stable"
+#define KERNEL_VERSION "Alpha-0.7.6-Stable"
 
 #endif
--- kernelspace/mm/kalloc.c
+++ kernelspace/mm/kalloc.c
@@ -2,55 +2,66 @@
 
 extern char end[]; // first address after kernel.
 
-struct run {
-  struct run *next;
-};
-
 struct {
-  struct run *freelist;
-  spinlock_t mem_lock;
+  spinlock_t lock;
+  unsigned char *bitmap;
+  uint64 start;
+  int npage;
 } kmem;
 
 void kinit() {
+  init_lock(&kmem.lock, "kmem");
   
-  init_lock(&kmem.mem_lock,"memlock");
+  kmem.start = (uint64)PGROUNDUP((uint64)end);
+  kmem.npage = (PHYSTOP - kmem.start) / PGSIZE;
+  
+  // Use the first page for the bitmap.
+  // A 4KB bitmap can manage 32768 pages (128MB).
+  kmem.bitmap = (unsigned char*)kmem.start;
+  
+  // Initially mark all pages as used (1).
+  memset(kmem.bitmap, 0xff, PGSIZE);
 
-  kmem.freelist = 0;
-  // free memory from end of kernel to PHYSTOP
-  for (char *p = (char*)PGROUNDUP((uint64)end); p + PGSIZE <= (char*)PHYSTOP; p += PGSIZE) {
-    kfree(p);
+  // Mark pages from bitmap's end to PHYSTOP as free.
+  // We skip the first page because it holds the bitmap.
+  for (uint64 p = kmem.start + PGSIZE; p + PGSIZE <= PHYSTOP; p += PGSIZE) {
+    kfree((void*)p);
   }
 }
 
 void kfree(void *pa) {
-  struct run *r;
+  uint64 addr = (uint64)pa;
 
-  if (((uint64)pa % PGSIZE) != 0 || (char*)pa < end || (uint64)pa >= PHYSTOP) {
+  if (addr % PGSIZE != 0 || addr < kmem.start || addr >= PHYSTOP) {
     return;
   }
 
-  r = (struct run*)pa;
+  int pageno = (addr - kmem.start) / PGSIZE;
+  int byte_idx = pageno / 8;
+  int bit_idx = pageno % 8;
 
-  accquire_lock(&kmem.mem_lock);
-
-  r->next = kmem.freelist;
-  kmem.freelist = r;
-
-  release_lock(&kmem.mem_lock);
+  accquire_lock(&kmem.lock);
+  kmem.bitmap[byte_idx] &= ~(1 << bit_idx);
+  release_lock(&kmem.lock);
 }
 
 void *kalloc() {
-  struct run *r;
+  accquire_lock(&kmem.lock);
 
-  accquire_lock(&kmem.mem_lock);
+  for (int i = 0; i < kmem.npage; i++) {
+    int byte_idx = i / 8;
+    int bit_idx = i % 8;
 
-  r = kmem.freelist;
-  if(r) {
-    kmem.freelist = r->next;
+    if (!(kmem.bitmap[byte_idx] & (1 << bit_idx))) {
+      kmem.bitmap[byte_idx] |= (1 << bit_idx);
+      release_lock(&kmem.lock);
+
+      void *pa = (void*)(kmem.start + (uint64)i * PGSIZE);
+      memset(pa, 5, PGSIZE); // Fill with junk to catch dangling refs
+      return pa;
+    }
   }
 
-  release_lock(&kmem.mem_lock);
-
-  return (void*)r;
-  
+  release_lock(&kmem.lock);
+  return 0;
 }
